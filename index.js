2b99ba998c679dc3326549fa7e17592b65bd4f37
Kylir <Kylir@users.noreply.github.com>
2017-06-23 18:29:54 +0100

    Add timelapse. Tested picture and video routes.

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
 
 const express = require('express')
 const spawn = require('child_process').spawn
+const path = require('path')
 
 var app = express()
 const PORT = 3000
+const FOLDER = 'public'
 
 app.get('/pic/:name', (req, res) => {
     let name = req.params.name
 
     const cmd = 'raspistill'
-    let args = ['-o', name]
+    let args = ['-o', FOLDER + '/' + name + '.jpg']
 
-    console.log(`Taking picture: ${cmd} ${args}`)
+    console.log(`Taking picture with command: ${cmd} ` + args.join(' '))
     var childProcess = spawn(cmd, args)
 
     childProcess.on('close', (code) => {
@@ -23,8 +25,30 @@ app.get('/pic/:name', (req, res) => {
 
     childProcess.on('error', (err) => {
         res.json({status:'ERROR', err: err})
-        console.error(err)            
+        console.error(err)
     })
+})
+
+app.get('/timelapse/:namePrefix/:duration/:interval', (req, res) => {
+    let prefix = req.params.namePrefix,
+        duration = req.params.duration, //TODO: add validation for integer
+        interval = req.params.interval
+
+    const cmd = 'raspistill'
+    let args = ['-o', FOLDER + '/' + prefix + '%04d.jpg', '-t', duration, '-tl', interval]
+
+    console.log(`Starting timelapse: ${cmd} ` + args.join(' '))
+    var childProcess = spawn(cmd, args)
+
+    childProcess.on('close', (code) => {
+        console.log(`${cmd} exited with code ${code}`)
+    })
+
+    childProcess.on('error', (err) => {
+        console.error(err)
+    })
+
+    res.json({status: 'OK'}) //no way to check it worked
 })
 
 app.get('/vid/:name/:time', (req, res) => {
@@ -32,20 +56,20 @@ app.get('/vid/:name/:time', (req, res) => {
     let time = req.params.time //TODO: add validation for integer
 
     const cmd = 'raspivid'
-    let args = ['-o', name, '-t', time]
+    let args = ['-o', FOLDER + '/' + name + '.h264', '-t', time]
 
-    console.log(`Taking picture: ${cmd} ${args}`)
+    console.log(`Recording video with command: ${cmd} ` + args.join(' '))
     var childProcess = spawn(cmd, args)
 
     childProcess.on('close', (code) => {
         console.log(`${cmd} exited with code ${code}`)
-        res.json({status: 'OK'})
     })
 
     childProcess.on('error', (err) => {
-        res.json({status:'ERROR', err: err})
         console.error(err)            
     })
+
+    res.json({status: 'OK'}) //no way to check it worked
 })
 
 app.listen(PORT, () => {
