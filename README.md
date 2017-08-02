d621ccdae0f28b55370195edbfc8fc71fa55fa2c
Kylir <Kylir@users.noreply.github.com>
2017-08-02 18:34:29 +0100

    update doc and switch to debug module

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Behind the scene it is using NodeJS (ExpressJS to be more specific.)
 ### Prerequisites
 
 I assume your Raspberry is already able to take pictures and record videos using `raspistill` and `raspivid`.
-If you're not sure have a look at the official documentation:
+If you're not sure, have a look at the official documentation:
 
 + [Enable the camera.](https://www.raspberrypi.org/documentation/usage/camera/README.md)
 + [Take pictures.](https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspistill.md)
@@ -20,35 +20,56 @@ If you're not sure have a look at the official documentation:
 
 I also assume you already have NodeJS and npm installed on your Pi. If not have a [look at this post on Stackoverflow](https://raspberrypi.stackexchange.com/a/48313/20530).
 
-Last assumption, to create an MP4 video from the raw H264 and to create a video from the timelapse images you need few command line tools: 
+Last assumption, to create an MP4 video from the raw H264 you need `MP4Box` to be installed.
 
-TODO: list the tools and the comand to install them. 
 
 ## Web Interface
 
-TODO
+TODO: for the moment the project has no web interface. But it would be nice to be able to take some pictures and record some videos remotely using a web page.
 
 
 ## API
 
-The web interface is using the API to do its job. If needed the interface can be skipped and the API can be contacted directly.
+The web interface is using the API to do its job. If needed the interface can be skipped and the API can be contacted directly. For instance you can reuse the API and write your own interface to interact with it.
 
 There are three routes so far:
 
 ### Take a picture
 
-Do a 
+To take a picture you need to call the `/pic` route and provide the correct `name` parameter.
 
 ```
 GET /pic/:name
 ```
 
+If the process is successfull, this will create a *name.jpg* picture in the `public` folder and return a JSON object:
+
+```json
+{"status":"OK", "type": "image", "path": "/content/name.jpg"}
+```
 
 
 ### Create a timelapse
 
-TODO
-https://www.raspberrypi.org/documentation/usage/camera/raspicam/timelapse.md
+You can create a series of images using the `/timelapse` route. You need to provide a `name prefix`, a `duration` (in seconds) and an `interval` (in seconds).
+
+```
+GET /timelapse/:name/:duration/:interval
+```
+
+Here is a bit more details:
+
++ name prefix: your images will start with this prefix and have a number from 000000 to 999999. For instance `picture0001.jpg`.
++ duration: how many seconds the camera will stay active.
++ interval: how many seconds between capturing two images.
+
+For instance to take a picture every 10 seconds for one hour and name the file pic000***.jpg you will have to call:
+
+```
+GET /timelapse/pic/3600/10
+```
+
+For more info about timelapse with the raspberry pi you can read [this page.](https://www.raspberrypi.org/documentation/usage/camera/raspicam/timelapse.md)
 
 ### Record a video
 
--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 const express = require('express')
 const spawn = require('child_process').spawn
 const path = require('path')
+const debug = require('debug')('pi-eye')
 
-var app = express()
+let app = express()
 const PORT = 3000
 const FOLDER = 'public'
 
@@ -14,23 +15,28 @@ app.use('/content', express.static(FOLDER))
 
 app.get('/pic/:name', (req, res) => {
     let name = req.params.name
+    let noError = true
 
     const cmd = 'raspistill'
     let args = ['-o', FOLDER + '/' + name + '.jpg']
 
-    console.log(`Taking picture with command: ${cmd} ` + args.join(' '))
-    var childProcess = spawn(cmd, args)
-
-    childProcess.on('close', (code) => {
-        console.log(`${cmd} exited with code ${code}`)
-        //redirect to the image
-        res.redirect(`/content/${name}.jpg`)
-    })
+    debug(`Taking picture with command: ${cmd} ` + args.join(' '))
+    let childProcess = spawn(cmd, args)
 
     childProcess.on('error', (err) => {
+        noError = false
         res.json({status:'ERROR', err: err})
         console.error(err)
     })
+
+    childProcess.on('close', (code) => {
+        if (noError) {
+            debug(`${cmd} exited with code ${code}`)
+            let imagePath = `/content/${name}.jpg`
+            res.json({status:'OK', type: 'image', path: imagePath})
+        }
+    })
+
 })
 
 app.get('/timelapse/:name/:duration/:interval', (req, res) => {
@@ -39,26 +45,26 @@ app.get('/timelapse/:name/:duration/:interval', (req, res) => {
         interval = req.params.interval * 1000 //TODO: add validation for integer
 
     const cmd = 'raspistill'
-    let args = ['-o', FOLDER + '/' + prefix + '%04d.jpg', '-t', duration, '-tl', interval]
     const options = {detached: true}
-
-    console.log(`Starting timelapse: ${cmd} ` + args.join(' '))
-    var childProcess = spawn(cmd, args, options)
+    let args = ['-o', FOLDER + '/' + prefix + '%06d.jpg', '-t', duration, '-tl', interval]
+    
+    debug(`Starting timelapse: ${cmd} ` + args.join(' '))
+    let childProcess = spawn(cmd, args, options)
 
     childProcess.on('close', (code) => {
-        console.log(`${cmd} exited with code ${code}`)
+        debug(`${cmd} exited with code ${code}`)
     })
 
     childProcess.on('error', (err) => {
         console.error(err)
     })
 
-    res.json({status: 'OK'}) //no way to check it worked
+    res.json({status: 'pending', type: 'timelapse', duration: duration, interval:interval}) //no way to check it worked
 })
 
 // convert to mp4: https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
 
-app.get('/vid/:name/:time', (req, res) => {
+app.get('/video/:name/:time', (req, res) => {
     const cmd = 'raspivid'
     const convCmd = 'MP4Box' //conversion to MP4
     
@@ -69,15 +75,20 @@ app.get('/vid/:name/:time', (req, res) => {
     const options = {detached: true}
     const convArgs = ['-add', FOLDER + '/' + name + '.h264', FOLDER + '/' + name + '.mp4']
 
-    console.log(`Recording video with command: ${cmd} ` + args.join(' '))
-    var childProcess = spawn(cmd, args, options)
+    debug(`Recording video with command: ${cmd} ` + args.join(' '))
+    let childProcess = spawn(cmd, args, options)
 
     childProcess.on('close', (code) => {
-        console.log(`${cmd} exited with code ${code}.`)
-        console.log(`Starting conversion to MP4 with command: ${convCmd} ` + convArgs.join(' '))
-        var conversionProcess = spawn(convCmd, convArgs)
+        debug(`${cmd} exited with code ${code}.`)
+        debug(`Starting conversion to MP4 with command: ${convCmd} ` + convArgs.join(' '))
+        let conversionProcess = spawn(convCmd, convArgs)
+        
         conversionProcess.on('close', (code) => {
-            console.log(`${convCmd} exited with code ${code}.`)
+            debug(`${convCmd} exited with code ${code}.`)
+        })
+
+        conversionProcess.on('error', (err) => {
+            console.error(err)
         })
     })
 
@@ -85,7 +96,7 @@ app.get('/vid/:name/:time', (req, res) => {
         console.error(err)            
     })
 
-    res.json({status: 'OK'}) //  Return early as there is no way to check if it worked.
+    res.json({status: 'pending', type: 'video', path: `/content/${name}.mp4`})
 })
 
 app.listen(PORT, () => {
