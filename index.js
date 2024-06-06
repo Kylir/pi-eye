271b894b4948273225d0b80e52314073feb5b607
Kylir <jason.forest@glyphe.org>
2024-06-06 13:39:14 +0100

    Change the conversion program to ffmpeg

--- index.js
+++ index.js
@@ -1,109 +1,144 @@
 /* jshint undef: true, node: true, eqeqeq: true, esnext: true, asi: true */
-'use strict'
+"use strict";
 
-const express = require('express')
-const spawn = require('child_process').spawn
-const debug = require('debug')('pi-eye')
+const express = require("express");
+const spawn = require("child_process").spawn;
+const debug = require("debug")("pi-eye");
 
-let app = express()
-const PORT = 3000
-const FOLDER = 'generated'
+let app = express();
+const PORT = 3000;
+const FOLDER = "generated";
 
 //content directory as static.
-app.use('/generated', express.static(FOLDER))
+app.use("/generated", express.static(FOLDER));
 
 // The web interface at the root of the site
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Take a picture with a given name
-app.get('/pic/:name', (req, res) => {
-    let name = req.params.name
-    let noError = true
+app.get("/pic/:name", (req, res) => {
+  let name = req.params.name;
+  let noError = true;
 
-    const cmd = 'raspistill'
-    let args = ['-o', FOLDER + '/' + name + '.jpg']
+  const cmd = "raspistill";
+  let args = ["-o", FOLDER + "/" + name + ".jpg"];
 
-    debug(`Taking picture with command: ${cmd} ` + args.join(' '))
-    let childProcess = spawn(cmd, args)
+  debug(`Taking picture with command: ${cmd} ` + args.join(" "));
+  let childProcess = spawn(cmd, args);
 
-    childProcess.on('error', (err) => {
-        noError = false
-        res.status(500).json({status:'error', err: err})
-        debug('Error received.')
-        console.error(err)
-    })
+  childProcess.on("error", (err) => {
+    noError = false;
+    res.status(500).json({ status: "error", err: err });
+    debug("Error received.");
+    console.error(err);
+  });
 
-    childProcess.on('close', (code) => {
-        if (noError) {
-            debug(`${cmd} exited with code ${code}. Sending ok to client.`)
-            let imagePath = `/generated/${name}.jpg`
-            res.json({status:'ok', type: 'image', path: imagePath})
-        }
-    })
+  childProcess.on("close", (code) => {
+    if (noError) {
+      debug(`${cmd} exited with code ${code}. Sending ok to client.`);
+      let imagePath = `/generated/${name}.jpg`;
+      res.json({ status: "ok", type: "image", path: imagePath });
+    }
+  });
+});
 
-})
+app.get("/timelapse/:name/:duration/:interval", (req, res) => {
+  let prefix = req.params.name,
+    duration = req.params.duration * 1000, //TODO: add validation for integer
+    interval = req.params.interval * 1000; //TODO: add validation for integer
 
-app.get('/timelapse/:name/:duration/:interval', (req, res) => {
-    let prefix = req.params.name,
-        duration = req.params.duration * 1000, //TODO: add validation for integer
-        interval = req.params.interval * 1000 //TODO: add validation for integer
+  const cmd = "raspistill";
+  const options = { detached: true };
+  let args = [
+    "-o",
+    FOLDER + "/" + prefix + "%06d.jpg",
+    "-t",
+    duration,
+    "-tl",
+    interval,
+  ];
 
-    const cmd = 'raspistill'
-    const options = {detached: true}
-    let args = ['-o', FOLDER + '/' + prefix + '%06d.jpg', '-t', duration, '-tl', interval]
-    
-    debug(`Starting timelapse: ${cmd} ` + args.join(' '))
-    let childProcess = spawn(cmd, args, options)
+  debug(`Starting timelapse: ${cmd} ` + args.join(" "));
+  let childProcess = spawn(cmd, args, options);
 
-    childProcess.on('close', (code) => {
-        debug(`${cmd} exited with code ${code}`)
-    })
+  childProcess.on("close", (code) => {
+    debug(`${cmd} exited with code ${code}`);
+  });
 
-    childProcess.on('error', (err) => {
-        console.error(err)
-    })
+  childProcess.on("error", (err) => {
+    console.error(err);
+  });
 
-    res.json({status: 'pending', type: 'timelapse', duration: duration, interval:interval}) //no way to check if it worked
-})
+  res.json({
+    status: "pending",
+    type: "timelapse",
+    duration: duration,
+    interval: interval,
+  }); //no way to check if it worked
+});
 
 // convert to mp4: https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
 // Start recording a video name :name for :time seconds
-app.get('/video/:name/:time', (req, res) => {
-    const cmd = 'raspivid'
-    const convCmd = 'MP4Box' //conversion to MP4
-    
-    let name = req.params.name
-    let time = req.params.time * 1000 //TODO: add validation for integer
+app.get("/video/:name/:time", (req, res) => {
+  const VIDEO_FRAME_RATE = 30;
+  const cmd = "raspivid";
+  const convCmd = "ffmpeg"; //conversion to MP4
 
-    const args = ['-o', FOLDER + '/' + name + '.h264', '-t', time]
-    const options = {detached: true}
-    const convArgs = ['-add', FOLDER + '/' + name + '.h264', FOLDER + '/' + name + '.mp4']
+  let name = req.params.name;
+  let time = req.params.time * 1000; //TODO: add validation for integer
 
-    debug(`Recording video with command: ${cmd} ` + args.join(' '))
-    let childProcess = spawn(cmd, args, options)
+  const args = [
+    "-o",
+    FOLDER + "/" + name + ".h264",
+    "-t",
+    time,
+    "-fps",
+    VIDEO_FRAME_RATE,
+  ];
+  const options = { detached: true };
+  // ffmpeg -framerate 24 -i input.264 -c copy output.mp4
+  const convArgs = [
+    "-framerate",
+    VIDEO_FRAME_RATE,
+    "-i",
+    FOLDER + "/" + name + ".h264",
+    "-c",
+    "copy",
+    FOLDER + "/" + name + ".mp4",
+  ];
 
-    childProcess.on('close', (code) => {
-        debug(`${cmd} exited with code ${code}.`)
-        debug(`Starting conversion to MP4 with command: ${convCmd} ` + convArgs.join(' '))
-        let conversionProcess = spawn(convCmd, convArgs)
-        
-        conversionProcess.on('close', (code) => {
-            debug(`${convCmd} exited with code ${code}.`)
-        })
+  debug(`Recording video with command: ${cmd} ` + args.join(" "));
+  let childProcess = spawn(cmd, args, options);
 
-        conversionProcess.on('error', (err) => {
-            console.error(err)
-        })
-    })
+  childProcess.on("close", (code) => {
+    debug(`${cmd} exited with code ${code}.`);
+    debug(
+      `Starting conversion to MP4 with command: ${convCmd} ` +
+        convArgs.join(" ")
+    );
+    let conversionProcess = spawn(convCmd, convArgs);
 
-    childProcess.on('error', (err) => {
-        console.error(err)            
-    })
+    conversionProcess.on("close", (code) => {
+      debug(`${convCmd} exited with code ${code}.`);
+    });
 
-    res.json({status: 'pending', type: 'video', path: `/generated/${name}.mp4`})
-})
+    conversionProcess.on("error", (err) => {
+      console.error(err);
+    });
+  });
+
+  childProcess.on("error", (err) => {
+    console.error(err);
+  });
+
+  res.json({
+    status: "pending",
+    type: "video",
+    path: `/generated/${name}.mp4`,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Starting Pi-Eye Camera server on port ${PORT}!`)
-    debug('Debug mode is ON!')
-})
+  console.log(`Starting Pi-Eye Camera server on port ${PORT}!`);
+  debug("Debug mode is ON!");
+});
